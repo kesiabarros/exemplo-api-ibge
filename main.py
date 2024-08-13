10752e55cb258dc9bacd753d4ad3556afc02defe
Késia S. de Barros <susanakesia@gmail.com>
2024-08-13 15:37:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests  # Importa a biblioteca requests, que permite fazer requisições HTTP
 
 # Define a URL do endpoint da API do IBGE para obter todos os municípios do estado de Mato Grosso (código 51)
-url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/35/municipios"
+url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/51/municipios"
 
 # Faz uma requisição GET para a URL especificada
 response = requests.get(url)
